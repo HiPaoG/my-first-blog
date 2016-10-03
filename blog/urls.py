3cd0aa6739b3fde946814df3776a5dc473c85b2e
Paola Gálvez <paola1995rgg@gmail.com>
2016-10-03 00:05:35 -0600

    Vistas agregadas para editar o crear dentro del sitio.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.articulo_lista)
+        url(r'^$', views.articulo_lista),
+        url(r'^post/(?P<pk>[0-9]+)/$', views.articulo_detalle),
 ]
